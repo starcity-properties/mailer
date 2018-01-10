0e62abc0cea8b74011c406197f3413670257045e
Josh Lehman <jalehman37@gmail.com>
2018-01-10 14:36:57 -0800

    Version 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/mailer "0.2.0"
+(defproject starcity/mailer "0.3.0-SNAPSHOT"
   :description "Mail service for Starcity."
   :url "https://github.com/starcity-properties/mailer"
   :license {:name "Eclipse Public License"
